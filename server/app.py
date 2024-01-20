eb016ff516a93244276378cbcd6d499712898164
fabian <farbean.makini@student.moringaschool.com>
2024-01-20 13:36:56 -0800

    backend done

--- server/app.py
+++ server/app.py
@@ -16,20 +16,6 @@ db.init_app(app)
 def test():
     return 'Pizzas Inn'
 
-@app.route('/pizzas')
-def pizzas():
-    pizzas = Pizza.query.all()
-    pizza_list = []
-    for pizza in pizzas:
-        pizza_dict = {
-            "id": pizza.id,
-            "name": pizza.name,
-            "ingredients": pizza.ingredients
-        }
-        pizza_list.append(pizza_dict)
-
-    return jsonify(pizza_list)
-
 @app.route('/restaurants')
 def restaurants():
     restaurants = Restaurant.query.all()
@@ -69,6 +55,64 @@ def get_restaurant(id):
     }
 
     return jsonify(restaurant_data), 200
+
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
+
+    RestaurantPizza.query.filter_by(restaurant_id=id).delete()
+
+    db.session.delete(restaurant)
+    db.session.commit()
+    return jsonify({}), 204
+
+@app.route('/pizzas')
+def pizzas():
+    pizzas = Pizza.query.all()
+    pizza_list = []
+    for pizza in pizzas:
+        pizza_dict = {
+            "id": pizza.id,
+            "name": pizza.name,
+            "ingredients": pizza.ingredients
+        }
+        pizza_list.append(pizza_dict)
+
+    return jsonify(pizza_list)
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    if not all(key in data for key in ['price', 'pizza_id', 'restaurant_id']):
+        return jsonify({"errors": ["Missing required fields"]}), 400
+
+    
+    pizza = Pizza.query.get(data['pizza_id'])
+    restaurant = Restaurant.query.get(data['restaurant_id'])
+
+    if not pizza or not restaurant:
+        return jsonify({"errors": ["Invalid Pizza or Restaurant ID"]}), 400
+    new_pizza = RestaurantPizza(
+        price=data['price'],
+        pizza_id=data['pizza_id'],
+        restaurant_id=data['restaurant_id']
+    )
+    try:
+        db.session.add(new_pizza)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"errors": ["Validation errors"]}), 400
+    pizza_data = {
+        "id": pizza.id,
+        "name": pizza.name,
+        "ingredients": pizza.ingredients
+    }
+
+    return jsonify(pizza_data), 201
 
 if __name__ == '__main__':
     app.run(port=5555)
