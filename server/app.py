e5443f6487871d99ea2819ff59702deb2cbceea7
fabian <farbean.makini@student.moringaschool.com>
2024-01-22 05:23:24 -0800

    front_end_init_commit

--- server/app.py
+++ server/app.py
@@ -2,15 +2,18 @@ from flask import Flask, request, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db , Pizza, Restaurant, RestaurantPizza
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
 
 @app.route('/')
 def test():
